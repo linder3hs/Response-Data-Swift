0f317676c138ec2c70e07c922b751757b0e2ea97
Linder Hassinger <linderhassinger00@gmail.com>
2018-01-27 23:36:38 -0500

    Initial Commit

---
+++ DataJsonTests/DataJsonTests.swift
@@ -0,0 +1,36 @@
+//
+//  DataJsonTests.swift
+//  DataJsonTests
+//
+//  Created by Linder Hassinger on 1/27/18.
+//  Copyright © 2018 Linder Hassinger. All rights reserved.
+//
+
+import XCTest
+@testable import DataJson
+
+class DataJsonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
